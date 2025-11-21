e84457603b680ed9cd5b82959345a8f64c86ef94
Victor <cuevasv341@gmail.com>
2025-11-20 19:32:07 -0500

    Cambio de color de las tarjetas del calendario

--- src/components/PartidosPoliticos/PartidosPoliticos.tsx
+++ src/components/PartidosPoliticos/PartidosPoliticos.tsx
@@ -178,7 +178,7 @@ const PartidosPoliticos: React.FC = () => {
               <line x1="12" y1="8" x2="12" y2="12"></line>
               <line x1="12" y1="16" x2="12.01" y2="16"></line>
             </svg>
-            {t('Partidos Filtros Propuestas')}
+            Filtrar por rubro de Propuesta
           </label>
           <select
             id="filtro-propuestas"
